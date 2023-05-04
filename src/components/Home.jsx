ba1cf0460308b61d38f0cb03a1f585942bb63719
Harold Manuel <aquinoharold460@gmail.com>
2023-05-03 21:38:25 -0400

    Adding the links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
               }}></TypeWritter></h2>
             <p className='text-[#8892b0] py-4 max-w-[700px] hover:text-white duration-100 transition-all ease-in-out'>I'm a Software Developer. Focused on building frontend apps and then, full-stack web applications in a near future.</p>
             <div>
-                <button className='text-white border-2 px-6 py-3 my-2 flex items-center justify-between hover:bg-pink-600 hover:border-pink-600'>View Work  <HiArrowNarrowRight/> </button>
+                <button className='text-white border-2 px-6 py-3 my-2 flex items-center justify-between hover:bg-pink-600 hover:border-pink-600'><Link to="work" smooth={true} duration={500}>View Work</Link><HiArrowNarrowRight/> </button>
             </div>
         </div>
     </main>
